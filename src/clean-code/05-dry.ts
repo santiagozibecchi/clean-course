f2fa12041e0ccba639aa2360456e99a3e438c607
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-11-08 17:13:15 -0300

    DRY in class Method

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -7,17 +7,37 @@ class Product {
       public size: Size = ""
    ) {}
 
+   // Aplicando principio DRY
+   isProductReady(): boolean {
+      for (const key in this) {
+         switch (typeof this[key]) {
+            case "string":
+               if ((<string>this[key]).length <= 0)
+                  throw Error(`${key} is empty`);
+               break;
+            case "number":
+               if (this[key] <= 0) throw Error(`${key} is zero`);
+               break;
+
+            default:
+               throw Error(`${typeof this[key]} is not supported`);
+         }
+      }
+      return true;
+   }
+
    toString() {
       // No DRY
-      if (this.name.length <= 0) throw Error("Name is empty");
-      if (this.price <= 0) throw Error("Price is zero");
-      if (this.size.length <= 0) throw Error("Size is empty");
+      //   if (this.name.length <= 0) throw Error("Name is empty");
+      //   if (this.price <= 0) throw Error("Price is zero");
+      //   if (this.size.length <= 0) throw Error("Size is empty");
+      if (!this.isProductReady()) return;
 
       return `${this.name} (${this.price}), ${this.size}`;
    }
 }
 
 (() => {
-   const bluePants = new Product("Pantalon negro", 100, "S");
+   const bluePants = new Product("Remera", 100, "S");
    console.log(bluePants.toString());
 })();
